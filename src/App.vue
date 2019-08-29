5a0b97cad40028d5a410e396ea45b0bd18f641ec
hex <31245112+hanson-hex@users.noreply.github.com>
2019-08-29 18:22:57 +0800

    修复了bug

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,53 @@
           </li>
       </ul>
       <Cart :name="name" :cart="cart"></Cart>
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+          <el-form-item label="用户名" prop="name">
+              <el-input  v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="pwd">
+              <el-input type="password" v-model="ruleForm.pwd"></el-input>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary" @click="submitForm()">提交</el-button>
+          </el-form-item>
+      </el-form>
+      <k-comp @bindclick="handleClick">
+          <strong>
+              学校
+          </strong>
+      </k-comp>
+      <!--<input type="text" v-model="inputText">-->
+      <!--<input type="text" :value="inputText" @input="inputText = $event.target.value">-->
+      <input type="text" :value="inputText" @input="handleInput">
   </div>
 </template>
 
 <script>
 import Cart from './components/Cart'
+import KComp from './components/KComp'
 import axios from 'axios'
 export default {
   name: 'app',
+  provide: {
+    name: 'hello world'
+  },
   components: {
-    Cart
+    Cart,
+    KComp
   },
   data () {
     return {
       txt: '',
+      inputText: '',
+      ruleForm: {
+        name: '',
+        pwd: ''
+      },
+      rules: {
+        name: { required: true, message: '请输入名称' },
+        pwd: { required: true, message: '请输入密码' }
+      },
       name: '测试',
       cart: [],
       goods: [
@@ -40,6 +73,23 @@ export default {
     // this.goods = res.data.list
   },
   methods: {
+    handleInput (e) {
+      console.log('e:', e)
+      this.inputText = e.target.value
+    },
+    handleClick (e) {
+      console.log('e:', e)
+    },
+    submitForm () {
+      this.$refs['ruleForm'].validate(valid => {
+        if (valid) {
+          alert('成功')
+        } else {
+          alert('大兄弟，你数据未完成')
+          return false
+        }
+      })
+    },
     addCart (i) {
       const good = this.goods[i]
       this.$bus.$emit('addCart', good)
---
+++ src/components/KComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="handleClick">
+        <slot></slot>
+        <span>{{ name }}</span>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'k-comp',
+  inject: ['name'],
+  methods: {
+    handleClick (e) {
+      this.$emit('bindclick', e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
